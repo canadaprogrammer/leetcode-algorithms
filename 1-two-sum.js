37fe5ffbf26d1242caa94dc3d4b9ab37f5587c46
JinPark1504 <jinpark1504@gmail.com>
2018-09-08 08:25:26 -0400

    added two-sum.js

---
+++ 1-two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++) {        
+        for(let j = i+1; j < nums.length; j++) {
+            let sum = nums[i] + nums[j];
+            if(sum === target) {
+                return [i, j];
+            }
+        }
+    }
+    return 'No solution!';
+};
+
+// Runtime: 116 ms
