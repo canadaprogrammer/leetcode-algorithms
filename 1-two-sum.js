94a0a7d75fccd76794cad4f151b2d2e7652fa198
JinPark1504 <jinpark1504@gmail.com>
2018-09-09 08:42:58 -0400

    Modified code for improving performance

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -3,16 +3,28 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++) {        
-        for(let j = i+1; j < nums.length; j++) {
-            let sum = nums[i] + nums[j];
-            if(sum === target) {
-                return [i, j];
-            }
+const twoSum = (nums, target) => {
+    let hash = {};
+    for(let i = 0; i < nums.length; i++) {
+        const j = target - nums[i];
+        if(typeof(hash[j]) !== 'undefined') {
+            return [hash[j], i];
         }
+        hash[nums[i]] = i;
     }
-    return 'No solution!';
-};
+    return [];
+}
+// Runtime: 52 ms
 
-// Runtime: 116 ms
+// const twoSum = (nums, target) => {
+//     for(let i = 0; i < nums.length; i++) {        
+//         for(let j = i+1; j < nums.length; j++) {
+//             let sum = nums[i] + nums[j];
+//             if(sum === target) {
+//                 return [i, j];
+//             }
+//         }
+//     }
+//     return [];
+// };
+// // Runtime: 116 ms
